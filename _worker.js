f3b543d903601357e5d1fda03188262f05a39c6c
Johnsheng1 <138360646+Johnsheng1@users.noreply.github.com>
2024-10-18 18:50:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4328,7 +4328,7 @@ var worker_default = {
               }
             });
           default:
-            url.hostname = "www.speedtest.net";
+            url.hostname = "b.j-s.us.kg";
             url.protocol = "https:";
             request = new Request(url, request);
             return await fetch(request);
